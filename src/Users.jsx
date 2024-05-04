a5bd51e189939185c4da61be5596f7f4a2ff2a7b
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-05-04 10:20:35 +0600

    after delete data initial refresh data method added

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
     const handleDelete = _id => {
         console.log(_id)
         fetch(`http://localhost:5000/users/${_id}`, {
@@ -12,6 +15,8 @@ const Users = () => {
                 console.log(data)
                 if (data.deletedCount > 0) {
                     alert("user delete successfully")
+                    const remaining = users.filter(user => user._id !== _id)
+                    setUsers(remaining)
                 }
             })
     }
