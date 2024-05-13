4ac53a86e811491533c9574a6842a182321583fa
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-05-13 22:01:36 +0600

    user info update method added

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const loadedUsers = useLoaderData();
@@ -30,6 +30,9 @@ const Users = () => {
                         {user.name}
                         {user.email}
                         {user._id}
+                        <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
                         <button
                             onClick={() => handleDelete(user._id)}
                         >X</button>
